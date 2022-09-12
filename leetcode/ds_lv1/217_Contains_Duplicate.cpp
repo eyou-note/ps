4004baf46cafc9c267fc9f347480b6df9fa1cab1
eazy-note <eoz.json@gmail.com>
2022-09-12 19:44:30 +0900

    217_Contains_Duplicate

---
+++ leetcode/ds_lv1/217_Contains_Duplicate.cpp
@@ -0,0 +1,15 @@
+
+bool containsDuplicate(vector<int>& nums) {
+
+    sort(nums.begin(), nums.end());
+    
+    int prev = nums[0];
+    for(int i=1;i<nums.size();i++){
+        if(prev == nums[i])
+            return true;
+        
+        prev = nums[i];
+    }
+
+    return false;
+}
