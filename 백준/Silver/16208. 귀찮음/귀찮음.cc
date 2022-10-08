1f722226bcba1867d90cd6b721eedb6e6a7fae92
Seok Woo Lee <103099148+eyou-note@users.noreply.github.com>
2022-10-09 08:59:21 +0900

    [Silver V] Title: 귀찮음, Time: 32 ms, Memory: 3920 KB -BaekjoonHub

--- 백준/Silver/16208. 귀찮음/귀찮음.cc
+++ 백준/Silver/16208. 귀찮음/귀찮음.cc
@@ -8,15 +8,15 @@ int main(){
     fastio;
     //freopen("input.txt", "r", stdin);
 
-    int n, sum=0;
-    long long cost=0; cin>>n;
+    int n;
+    long long sum=0,cost=0; cin>>n;
     vector<int> a(n);
     for(int i =0;i<n;i++){
         cin>>a[i];
         sum+=a[i];
     }
 
-    sort(a.begin(), a.end());
+    // sort(a.begin(), a.end());
 
     for(int i=0;i<n;i++){
         sum-=a[i];
