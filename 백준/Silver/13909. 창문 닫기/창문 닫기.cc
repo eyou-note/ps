017ce593e8a52c9571f748778bff5128ebcc550b
Seok Woo Lee <103099148+eyou-note@users.noreply.github.com>
2022-10-12 20:56:21 +0900

    [Silver V] Title: 창문 닫기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+using namespace std;
+
+int main(){
+    fastio;
+   // freopen("input.txt", "r", stdin);
+
+    int n; cin>>n;
+    cout<<(int)sqrt(n);
+
+    return 0;
+}
+
