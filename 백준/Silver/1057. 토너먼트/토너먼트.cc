c76b5bf09f5501155af3e6f2698c307a1ed859df
Seok Woo Lee <103099148+eyou-note@users.noreply.github.com>
2022-10-17 14:54:40 +0900

    [Silver IV] Title: 토너먼트, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1057. 토너먼트/토너먼트.cc
+++ 백준/Silver/1057. 토너먼트/토너먼트.cc
@@ -8,32 +8,14 @@ int main(){
     fastio;
     //freopen("input.txt", "r", stdin);
 
-    int n,a,b,round=1; cin>>n>>a>>b;
-    vector<int> v(n+1);
-    for(int i=1;i<=n;i++){
-        v[i] = i;
-    }
+    int n,a,b,round=0; cin>>n>>a>>b;
 
-    while(1){
-        vector<int> result; result.push_back(0);
-        bool match = false;
-        for(int i=1;i<v.size();i+=2){
-            bool flag = i+1<v.size();
-            if( i+1 < v.size() &&  ((v[i] == a && v[i+1] ==b) || ((v[i] == b && v[i+1] ==a))) ){ 
-                match = true;
-                break;
-            }
-
-            if(v[i] == a || v[i] == b) result.push_back(v[i]);
-            else result.push_back( i+1 < v.size() ? v[i+1] : v[i]);
-        }
-        if(match) break;
-
+    while(a!=b){
+        a -= (a/2);
+        b -= (b/2);
         round+=1;
-
-        v = result;
     }
-    
+
     cout<<round;
 
     return 0;
