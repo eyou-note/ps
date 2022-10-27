9d99db655e1dfab8b26bf78ceb28c32f7e07a4ff
Seok Woo Lee <103099148+eyou-note@users.noreply.github.com>
2022-10-27 09:27:45 +0900

    [Silver IV] Title: 도로와 신호등, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2980. 도로와 신호등/도로와 신호등.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+using namespace std;
+
+typedef struct _Light{
+    int pos;
+    int red;
+    int green;
+} Light;
+
+int main(){
+    fastio;
+    //freopen("input.txt", "r", stdin); 
+
+    int N,L,D,R,G,time=0,idx=0;
+    cin>>N>>L;
+
+    vector<Light> lights(N);
+    for(int i=0;i<N;i++){
+        cin>>D>>R>>G;
+        lights[i] = {D,R,G};
+    }
+
+    for(int i=1;i<=L;i++){
+        if(i == lights[idx].pos){
+            int r = time%(lights[idx].red + lights[idx].green);
+            if(r < lights[idx].red){
+                time += lights[idx].red -r+1;
+            }else{
+                time += 1;
+            }
+            idx+=1;
+        }else{
+            time+=1;
+        }
+    }
+
+    cout<<time-1;
+
+
+    
+    return 0;
+}
