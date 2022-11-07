a2f40443a3405ee075fcc1ce22565616d2cf3e3b
Seok Woo Lee <103099148+eyou-note@users.noreply.github.com>
2022-11-08 07:26:52 +0900

    [Silver IV] Title: 청기 백기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/15736. 청기 백기/청기 백기.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+using namespace std;
+
+int main(){
+    fastio;
+   // freopen("input.txt", "r", stdin);  
+
+    int n; cin>>n;
+    int ans = (int)sqrt(n);
+
+    cout<<ans;
+
+    return 0;
+}
