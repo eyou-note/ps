99b23479cd74cb8ab583c766b6ebe3eac6be92e2
Seok Woo Lee <103099148+eyou-note@users.noreply.github.com>
2022-11-17 09:04:18 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define endl "\n"
+
+using namespace std;
+
+int n, m; 
+int a[8], b[8];
+bool chk[8];
+
+void dfs(int idx, int cnt){
+    if(cnt==m){
+        for(int i=0;i<m;i++){
+            cout<<b[i]<<' ';
+        }
+        cout<<endl;
+        return;
+    }
+
+    for(int i=idx;i<n;i++){
+        b[cnt] = a[i];
+        dfs(idx, cnt+1);
+        idx+=1;
+    }   
+}
+
+
+int main(){
+    fastio;
+    //("input.txt", "r", stdin);  
+
+    cin>>n>>m;
+
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    sort(a, a+n);
+    
+    dfs(0, 0);
+
+    return 0;
+
+}
