67505ec6459ab29c48e2bcd3841f26a62c44a6ad
Seok Woo Lee <103099148+eyou-note@users.noreply.github.com>
2022-11-24 09:17:02 +0900

    [Silver III] Title: Four Squares, Time: 12 ms, Memory: 2264 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.cc
+++ 백준/Silver/17626. Four Squares/Four Squares.cc
@@ -14,7 +14,7 @@ int main(){
 
     dp[1] = 1;
     for(int i=1;i<=n;i++){
-        dp[i] = dp[1] + dp[i-1];
+        dp[i] = dp[i-1]+1;
         for(int j=2;j*j<=i;j++){
             dp[i] = min(dp[i], dp[i-(j*j)]+1);
         }
