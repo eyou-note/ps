c69c026d7329139d3d7229237266babd5a540e14
Seok Woo Lee <103099148+eyou-note@users.noreply.github.com>
2022-11-24 09:15:49 +0900

    [Silver III] Title: Four Squares, Time: 16 ms, Memory: 2264 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define endl "\n"
+
+using namespace std;
+
+int main(){
+    fastio;
+    //freopen("input.txt", "r", stdin);  
+
+    int n; cin>>n;
+    vector<int> dp(n+1,0);
+
+    dp[1] = 1;
+    for(int i=1;i<=n;i++){
+        dp[i] = dp[1] + dp[i-1];
+        for(int j=2;j*j<=i;j++){
+            dp[i] = min(dp[i], dp[i-(j*j)]+1);
+        }
+    }
+
+    cout<<dp[n];
+    
+
+    return 0;
+}
