59d197e99797afd49f711a25c4a01b85cefc85aa
Seok Woo Lee <103099148+eyou-note@users.noreply.github.com>
2023-04-11 19:55:28 +0900

    [unrated] Title: 숫자 변환하기, Time: 0.33 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.cpp
+++ 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.cpp
@@ -3,28 +3,44 @@
 
 using namespace std;
 int solution(int x, int y, int n) {
-	int cache[1000001]={0,};
-		
-	for(int i=x;i<=y;i++){
-		if(i!=x  && cache[i] == 0){
-			 cache[i] = -1;
-			 continue;
-		}
+    
+    if(x==y) return 0;
+    
+    int ans = 0;
+    queue<pair<int,int>> q;
+    set<int> st;
+    st.insert(x);
+    q.push({x,0});
 
-		if(i*2 <= y){
-			cache[i * 2] = (cache[i * 2] == 0) ? cache[i] + 1 : min(cache[i] + 1, cache[i * 2]);
-		}
 
-		if(i*3 <= y){
-			cache[i * 3] = (cache[i * 3] == 0) ? cache[i] + 1 : min(cache[i] + 1, cache[i * 3]);
-		}
 
-		if(i+n <= y){
-			cache[i + n] = (cache[i + n] == 0) ? cache[i] + 1 : min(cache[i] + 1, cache[i + n]);
-		}
-		
-	}
+    while(!q.empty()){
+        pair<int, int> cur = q.front();
+        q.pop();
 
-	return cache[y];
+        if(cur.first == y){
+            ans = cur.second;
+            break;
+        }
+        else if(cur.first < y){
+
+            int x2 = cur.first * 2;
+            if(st.insert(x2).second){
+                q.push({x2, cur.second+1});
+            }
+
+            int x3 = cur.first * 3;
+            if(st.insert(x3).second){
+                q.push({x3, cur.second+1});
+            }
+
+
+            int add = cur.first + n;
+            if(st.insert(add).second){
+                q.push({add, cur.second+1});
+            }
+        }
+    }
+
+	return ans == 0 ? -1 : ans;
 }
-
