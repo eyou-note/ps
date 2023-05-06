beca9cafca0a91f1d0f1726fb668b08626e16e5f
Seok Woo Lee <103099148+eyou-note@users.noreply.github.com>
2023-05-06 15:58:42 +0900

    [unrated] Title: 추억 점수, Time: 0.02 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.cpp
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.cpp
@@ -1,24 +1,23 @@
-#include <string>
-#include <vector>
-#include <map>
+#include <bits/stdc++.h>
 
 using namespace std;
-vector<int> solution(vector<string> name, vector<int> point, vector<vector<string>> photos) {
+
+vector<int> solution(vector<string> name, vector<int> yearning, vector<vector<string>> photo) {
     vector<int> ans;
-    map<string,int> name_point_map;
+	map<string, int> name_map;
 
-    for(int i=0;i<name.size();i++){
-        name_point_map[name[i]] = point[i];
-    }
+	for(int i=0;i<name.size();i++){
+		name_map.insert({name[i], yearning[i]});	
+	}
 
-    for(auto photo: photos){
-        int sum = 0;
-        for(auto p : photo){
-            map<string,int>::iterator it =  name_point_map.find(p);
-            if(it!=name_point_map.end()) sum += it->second;
-        }
-        
-        ans.push_back(sum);
-    }
+	for(auto p : photo){
+		int sum = 0;
+		for(auto t : p){
+			if(name_map.find(t) != name_map.end())
+				sum+=name_map.find(t)->second;
+		}
+		ans.push_back(sum);
+	}
+
     return ans;
 }
