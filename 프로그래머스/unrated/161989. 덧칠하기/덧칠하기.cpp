27c6d2d589206e28e930ce6322e0defa1c8e20d9
Seok Woo Lee <103099148+eyou-note@users.noreply.github.com>
2023-05-06 16:33:53 +0900

    [unrated] Title: 덧칠하기, Time: 0.16 ms, Memory: 4.99 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.cpp
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.cpp
@@ -3,24 +3,15 @@
 
 using namespace std;
 
-
 int solution(int n, int m, vector<int> section) {
-    int cnt = 0;
-    for(int i=0;i<section.size();i++){
-        if(section[i]==0) continue;
-        
-        int begin = section[i], 
-            end = begin + m -1;
+    int cnt = 0, end = 0;
 
-        for(int j=i;j<section.size();j++){
-            if(section[j] <= end)
-                section[j] = 0;
-            else
-                break;
-        }
-        cnt+=1;
-    }
+	for(int i=0;i<section.size();i++){
+		if(end<=section[i]){
+			end = section[i] + m;
+			cnt+=1;
+		}
+	}
 
     return cnt;
 }
-
