167f1869ea32947d415d46cb4f46c72557e574b9
Seok Woo Lee <103099148+eyou-note@users.noreply.github.com>
2023-05-06 15:47:38 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+#define endl '\n'
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+using namespace std;
+
+int main(){
+    fastio;
+   // freopen("input.txt", "r", stdin);
+
+	int r1, s;
+	cin>>r1>>s;
+
+	cout<<(2*s) - r1;
+
+    return 0;
+}
