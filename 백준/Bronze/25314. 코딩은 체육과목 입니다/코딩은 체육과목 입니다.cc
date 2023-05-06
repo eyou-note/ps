86eba74212f7217a576f2c9988c88d3f06f0b071
Seok Woo Lee <103099148+eyou-note@users.noreply.github.com>
2023-05-06 14:07:53 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+#define endl '\n'
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+using namespace std;
+
+int main(){
+    fastio;
+   // freopen("input.txt", "r", stdin);
+
+	int n,r; cin>>n;
+
+	r = n/4;
+
+	for(int i=0;i<r;i++){
+		cout<<"long ";
+	}
+
+	cout<<"int";
+
+    return 0;
+}
